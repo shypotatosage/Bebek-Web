c5455e42ef7e65c4d6f72a3e7449fd42b768748f
dhammikod <63945345+dhammikod@users.noreply.github.com>
2023-05-14 05:31:02 +0700

    update profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -22,7 +22,43 @@
                         @csrf
                         <button type="submit" class="btn btn-danger font-montserrat fw-semibold py-2 px-4">Logout</button>
                     </form>
+                    <button type="button" class="btn btn-warning font-montserrat fw-semibold py-2 px-4" data-bs-toggle="modal"
+                        data-bs-target="#addNewAdmin">+
+                        Edit profile</button>
                 @endauth
+            </div>
+        </div>
+        <div class="modal fade" id="addNewAdmin" tabindex="-1">
+            <div class="modal-dialog">
+                <div class="modal-content">
+
+                    <form method="POST" action="/updateuser" enctype="multipart/form-data">
+                        @csrf
+                        <input type="hidden" name="id" value="{{ $user->id }}">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Edit profile</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <label for="category">Email</label>
+                            <input type="text" class="form-control" id="category" value="{{ $user->name }}"
+                                name="email" required>
+                            <label for="phone">Phone number</label>
+                            <input type="text" class="form-control" id="phone" value="{{ $user->phone_number }}"
+                                name="phone" required>
+                            <label for="bank">Bank account number</label>
+                            <input type="text" class="form-control" id="bank" value="{{ $user->bank_account_number }}"
+                                name="bank" required>
+
+
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <button type="submit" class="btn btn-success">Add</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
