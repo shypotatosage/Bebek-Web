ba5558786ab2a668ab5baf2e08acd7bc83765001
dhammikod <63945345+dhammikod@users.noreply.github.com>
2023-05-13 18:00:07 +0700

    dashboard page
    +backend

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bazaar;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function dashboard(){
+        $items = Bazaar::select('*')
+        ->limit(8)
+        ->get();;
+        return view('dashboard', [
+            'pagetitle' => 'Catalog',
+            'items' => $items
+        ]);
+    }
 }
