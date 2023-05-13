6d455110243ca8e6cc54fb3a1ed4c4e8535f1310
Mimi <91705093+shypotatosage@users.noreply.github.com>
2023-05-13 14:00:04 +0700

    Login Page UI Done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,52 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('styles/loginregister.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Login</title>
+</head>
+<body class="login-register-bg">
+    <div class="d-flex flex-row justify-content-lg-start justify-content-center py-lg-0 py-4 logreg-container">
+        <div class="login-register-form-bg d-flex align-items-center justify-content-center flex-column">
+            <h2 class="fw-bold font-montserrat">MASUK</h2>
+            <div class="line1 mx-4"></div>
+            <h5 class="font-montserrat fw-semibold mt-3 text-center mx-5">Promote Your <span class="color-yellow">Start Up</span> And Gain <span class="color-yellow">Investors</span></h5>
+            <form action="{{ route('login') }}" method="post" class="w-75">
+                @csrf
+                <div class="mb-3 mt-4">
+                    <input type="email" class="form-control font-montserrat" id="inputEmail" name="email" placeholder="Email" required autofocus>
+                    <small class="text-danger font-montserrat">
+                    @foreach ($errors->get('email') as $err)
+                        {{ $err }}
+                    @endforeach
+                    </small>
+                </div>
+                <div class="mb-3 mt-4">
+                    <input type="password" class="form-control font-montserrat" id="inputPassword" name="password" placeholder="Kata Sandi" required autocomplete="current-password">
+                    <small class="text-danger font-montserrat">
+                    @foreach ($errors->get('password') as $err)
+                        {{ $err }}
+                    @endforeach
+                    </small>
+                </div>
+                <div class="mb-4">
+                <div class="form-check">
+                    <input class="form-check-input loginregistercheckbox" type="checkbox" id="rememberLoginCheck" name="remember">
+                    <label class="form-check-label" for="rememberLoginCheck">
+                    Ingat Saya
+                    </label>
+                </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button type="submit" class="btn btn-primary font-montserrat fw-semibold py-2 px-4">Masuk</button>
+                </div>
+            </form>
+            <h6 class="font-montserrat fw-semibold mt-3 mx-4 text-center">Belum mempunyai akun? <span class="color-yellow fw-bold"><a href="{{ route('register') }}" class="text-decoration-none color-yellow">Daftar Di Sini!</a></span></h6>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
