343de77c8437adfb8092dbebc1bfb615d94f0e48
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2023-05-13 19:50:45 +0700

    Creating BazaarResources

---
+++ app/Filament/Resources/BazaarResource/Pages/EditBazaar.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\BazaarResource\Pages;
+
+use App\Filament\Resources\BazaarResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditBazaar extends EditRecord
+{
+    protected static string $resource = BazaarResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
